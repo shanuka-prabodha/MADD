28a5ad4377a8307f8ba2aaf7d24f398f4db4d260
Shanuka Prabodha <shanukaprabodha1262@gmail.com>
2020-08-01 19:24:13 +0530

    initial commit

---
+++ app/src/main/java/com/spd/it19207100/MainActivity.java
@@ -0,0 +1,14 @@
+package com.spd.it19207100;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
